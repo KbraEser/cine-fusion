63ea0f0f8936140775d5c820d5660ecceb0fae98
KbraEser <kbra.demirbas@gmail.comm>
2025-05-19 10:21:29 +0300

    Redux ile film detaylarını çekmek için yeni bir dilim eklendi. `PopularMovieDetail` bileşeni güncellendi  .hata yönetimi için toast bildirimleri entegre edildi.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import popularMoviesReducer from "./slices/popularMoviesSlice";
 import languageReducer from "./slices/languageSlice";
+import movieDetailsReducer from "./slices/movieDetailsSlice";
 
 export const store = configureStore({
   reducer: {
     popularMovies: popularMoviesReducer,
     language: languageReducer,
+    movieDetails: movieDetailsReducer,
   },
 });
 
